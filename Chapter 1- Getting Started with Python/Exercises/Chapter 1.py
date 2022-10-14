0311eed80a0389f700d389818b27ecd575ddcdd2
Adhenz Miranda <75351066+adhenzmiranda@users.noreply.github.com>
2022-10-14 21:44:44 +0400

    re-added exercise 5

--- Chapter 1- Getting Started with Python/Exercises/Chapter 1.py
+++ Chapter 1- Getting Started with Python/Exercises/Chapter 1.py
@@ -20,3 +20,8 @@ name = "Adhenz Miranda"
 Course = "Creative Computing Year 1"
 Age = "17"
 print ("Name:", name, "Course:", Course, "Age:", Age)
+
+#exercise 5
+from math import pi
+radius = float(input ("Input the radius of the circle to find the area : "))
+print ("The initial area of the circle with the radius of " + str(radius) + " is: " + str(pi * radius**2))
